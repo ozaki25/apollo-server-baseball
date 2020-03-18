fb2c1417570a0ad4c0b3568aec5819c935e90d1c
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-03-18 18:50:05 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
