da6136ced95291d1e8b2f3f8e04cfc5b8f40342c
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-03-18 19:51:02 +0900

    update data

--- data/teams.js
+++ data/teams.js
@@ -8,69 +8,69 @@ const teams = [
   {
     name: '横浜DeNAベイスターズ',
     englishName: 'YOKOHAMA DeNA BAYSTARS',
-    foundingDate: '1949年12月15日創立',
+    foundingDate: '1949年12月15日',
     homeStadium: '横浜スタジアム',
   },
   {
     name: '阪神タイガース',
     englishName: 'Hanshin Tigers',
-    foundingDate: '1935年12月10日創立',
+    foundingDate: '1935年12月10日',
     homeStadium: '阪神甲子園球場',
   },
   {
     name: '広島東洋カープ',
     englishName: 'Hiroshima Toyo Carp',
-    foundingDate: '1949年12月5日創立',
+    foundingDate: '1949年12月5日',
     homeStadium: 'MAZDA Zoom-Zoom スタジアム広島',
   },
   {
     name: '中日ドラゴンズ',
     englishName: 'Chunichi Dragons',
-    foundingDate: '1936年1月15日創立',
+    foundingDate: '1936年1月15日',
     homeStadium: 'ナゴヤドーム',
   },
   {
     name: '東京ヤクルトスワローズ',
     englishName: 'Tokyo Yakult Swallows',
-    foundingDate: '1950年1月12日加盟',
+    foundingDate: '1950年1月12日',
     homeStadium: '明治神宮野球場',
   },
   {
     name: '埼玉西武ライオンズ',
     englishName: 'Saitama Seibu Lions',
-    foundingDate: '1949年11月26日加盟',
+    foundingDate: '1949年11月26日',
     homeStadium: 'メットライフドーム',
   },
   {
     name: '福岡ソフトバンクホークス',
     englishName: 'Fukuoka SoftBank Hawks',
-    foundingDate: '1938年2月22日創立',
+    foundingDate: '1938年2月22日',
     homeStadium: '福岡PayPayドーム',
   },
   {
     name: '東北楽天ゴールデンイーグルス',
     englishName: 'Tohoku Rakuten Golden Eagles',
-    foundingDate: '2004年11月2日加盟',
+    foundingDate: '2004年11月2日',
     homeStadium: '楽天生命パーク宮城',
   },
   {
     name: '千葉ロッテマリーンズ',
     englishName: 'Chiba Lotte Marines',
-    foundingDate: '1949年11月26日加盟',
+    foundingDate: '1949年11月26日',
     homeStadium: 'ZOZOマリンスタジアム',
   },
   {
     name: '北海道日本ハムファイターズ',
     englishName: 'Hokkaido Nippon-Ham Fighters',
-    foundingDate: '1945年11月6日加盟',
+    foundingDate: '1945年11月6日',
     homeStadium: '札幌ドーム',
   },
   {
     name: 'オリックス・バファローズ',
     englishName: 'ORIX Buffaloes',
-    foundingDate: '1936年1月23日創立',
+    foundingDate: '1936年1月23日',
     homeStadium: '京セラドーム大阪',
   },
 ];
 
-module.exports = books;
+module.exports = teams;
