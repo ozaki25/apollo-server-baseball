e83a4cb8e74f57f35613e8bb0ad88f1d606d132b
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-19 23:18:35 +0900

    add cors settings

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./src/schema');
 const resolvers = require('./src/resolver');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: true,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
